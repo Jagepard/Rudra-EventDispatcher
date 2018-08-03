ae87546c137b50ccffac637450e1f9cd8aed18bf
Jagepard <dankorot@gmail.com>
2018-08-03 11:43:22 +0300

    Update

--- tests/stub/Events/AppEvents.php
+++ tests/stub/Events/AppEvents.php
@@ -6,5 +6,8 @@ class AppEvents
 {
     const APP_CLOSURE  = 'app.closure';
     const APP_LISTENER = 'app.listener';
+
+    const SUB_CLOSURE  = 'sub.closure';
+    const SUB_LISTENER = 'sub.listener';
 }
 
