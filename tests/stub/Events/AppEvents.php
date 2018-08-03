e497159c7368d0b339d6ac38937abd8b364e5e65
Jagepard <dankorot@gmail.com>
2018-08-03 11:39:19 +0300

    style update

--- tests/stub/Events/AppEvents.php
+++ tests/stub/Events/AppEvents.php
@@ -4,7 +4,7 @@ namespace Rudra\Tests\stub\Events;
 
 class AppEvents
 {
+    const APP_CLOSURE  = 'app.closure';
     const APP_LISTENER = 'app.listener';
-    const APP_CLOSURE = 'app.closure';
 }
 
