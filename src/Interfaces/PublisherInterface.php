43dc06f6dcc5eb30c0da1f115bdb360eb99acf39
Jagepard <dankorot@gmail.com>
2018-08-06 17:20:58 +0300

    Add PublisherInterface

---
+++ src/Interfaces/PublisherInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2018, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPL-3.0
+ */
+
+namespace Rudra\Interfaces;
+
+/**
+ * Interface PublisherInterface
+ * @package Rudra\Interfaces
+ */
+interface PublisherInterface
+{
+
+    /**
+     * @param string              $event
+     * @param SubscriberInterface $subscriber
+     */
+    public function attachSubscriber(string $event, SubscriberInterface $subscriber): void;
+
+    /**
+     * @param string              $event
+     * @param SubscriberInterface $subscriber
+     */
+    public function detachSubscriber(string $event, SubscriberInterface $subscriber): void;
+
+    /**
+     * @param string $event
+     */
+    public function notify(string $event): void;
+}
