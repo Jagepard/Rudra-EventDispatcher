108099e772db5c22a698bfac3283fd79f4991219
Jagepard <dankorot@gmail.com>
2019-01-14 16:03:44 +0300

    Add APP_PARAMS

--- tests/stub/Events/AppEvents.php
+++ tests/stub/Events/AppEvents.php
@@ -6,6 +6,7 @@ class AppEvents
 {
     const APP_CLOSURE  = 'app.closure';
     const APP_LISTENER = 'app.listener';
+    const APP_PARAMS   = 'app.params';
 
     const SUB_CLOSURE  = 'sub.closure';
     const SUB_LISTENER = 'sub.listener';
