355c668ab369dbf613e1d83a82e1022a9a6f8bc2
Jagepard <dankorot@gmail.com>
2021-01-13 10:55:35 +0300

    add getListeners

--- src/EventDispatcherFacade.php
+++ src/EventDispatcherFacade.php
@@ -12,6 +12,7 @@ use Rudra\Container\Traits\FacadeTrait;
 /**
  * @method static void addListener(string $event, $listener, ...$arguments)
  * @method static dispatch(string $event, ...$arguments)
+ * @method static array getListeners()
  *
  * @method static void attachObserver(string $publisher, string $event, $subscriber, ...$arguments)
  * @method static void detachObserver(string $publisher, string $event, string $subscriberName)
